f9219e79a4a783e2ad14c798c61142fecff7fd6e
Mike Gossmann <mike@c572.ca>
2012-11-11 17:39:01 -0500

    Merges the functionality from Actors into Gobs
    Actors were infinitely more useful than Gobs, but lacked the
    crucial ability to have children. Gobs could nest but had no
    support for features. The Actors' support for features and flexible
    events has been moved to Gobs, and Actors have been removed.

--- aukera.js
+++ aukera.js
@@ -3,10 +3,11 @@
 
 /*
  * ---------------------------------------------------------------------------
- *  Game.js
- *  This file creates the core of the game engine, including the actor
- *  listing, and update loop. Room objects are also included here, since they
- *  are a core part of the game engine.
+ *  Aukera.js
+ *  This file creates the core of the game engine, the Gob object. Gobs are
+ *  flexible objects that features can be added to in order to create the
+ *  objects in a game. This file also contains a special Game object, which is
+ *  a Gob with methods for running the game loop.
  * ---------------------------------------------------------------------------
  */
 
@@ -21,24 +22,52 @@ auk.modules = [];
 /**
  * The Gob object is an array of gobs with some extra methods for handling it's
  * children, including adding, removing, and passing along events.
+ * @param {int} x      The Gob's X position in px.
+ * @param {int} y      The Gob's Y position in px.
+ * @param {int} z      The Gob's X position in px.
+ * @param {int} width  The Gob's width in px.
+ * @param {int} height The Gob's height in px.
  */
-auk.Gob = function () {};
+auk.Gob = function (x, y, z, width, height) {
+	// Position the Gob
+	this.x = x;
+	this.y = y;
+	this.z = z;
+	this.width = width;
+	this.height = height;
+
+	// Game integration
+	this.game = null;
+
+	// feature mixin support
+	this.eventSteps = {};
+
+	this.childSteps = [];
+	this.orphanSteps = [];
+};
 auk.Gob.prototype = [];
 
 /**
- * Dispatches a given game event to the appropriate method, and then calls
- * fireGameEvent again on all child gobs. If the children are not Gobs, it falls
- * back on calling a function with the event name.
+ * Dispatches a given game event to the appropriate event step methods, and then
+ * calls fireGameEvent again on all child gobs. If the children are not Gobs, it
+ * falls back on calling a function with the event name.
  *
- * @param  {string} eventName The name of the event to trigger
+ * @param {string} eventName The name of the event to trigger.
+ * @param {bool}   shallow   If true, the function will not recurse.
  */
-auk.Gob.prototype.fireGameEvent = function (eventName) {
+auk.Gob.prototype.fireGameEvent = function (eventName, shallow) {
 	var gobCount = this.length;
 	var gob;
+	var steps;
+	var stepCount;
 	var i;
 
-	if (this[eventName]) {
-		this[eventName]();
+	if (this.eventSteps[eventName]) {
+		steps = this.eventSteps[eventName];
+		stepCount = steps.length;
+		for (i = 0; i < stepCount; i += 1) {
+			steps[i].call(this);
+		}
 	}
 
 	for (i = 0; i < gobCount; i += 1) {
@@ -54,6 +83,73 @@ auk.Gob.prototype.fireGameEvent = function (eventName) {
 };
 
 /**
+ * Adds a new step function to execute for a given game event.
+ *
+ * @param {string}   eventName    The name of the event to add the function to.
+ * @param {function} stepFunction The function to call.
+ */
+auk.Gob.prototype.addEventStep = function (eventName, stepFunction) {
+	if (!this.eventSteps.hasOwnProperty(eventName)) {
+		this.eventSteps[eventName] = [];
+	}
+	this.eventSteps[eventName].push(stepFunction);
+	return this;
+};
+
+/**
+ * Removes the given function from the given event.
+ *
+ * @param  {sting}    eventName    The name of the event to remove the function
+ *                                 from.
+ * @param  {function} stepFunction The function to remove.
+ */
+auk.Gob.prototype.removeEventStep = function (eventName, stepFunction) {
+	var functionIndex;
+	if (this.eventSteps[eventName]) {
+		functionIndex = this.eventSteps[eventName].indexOf(stepFunction);
+		if (functionIndex !== -1) {
+			this.eventSteps[eventName].splice(functionIndex, 1);
+		}
+	}
+	return this;
+};
+
+/**
+ * Adds features to the object
+ *
+ * This is accomplished by calling the feature's addTo function on
+ * this object. Using add() on the Gob allows chaining.
+ *
+ * @param feature: the feature to add
+ * @param options: An options object to pass to the addTo function.
+ */
+auk.Gob.prototype.addFeature = function (feature, options) {
+
+	if (feature.addTo) {
+		feature.addTo(this, options);
+	}
+
+	return this;
+};
+
+/**
+ * Removes a feature from the object
+ *
+ * This is accomplished by calling the features removeFrom function with this
+ * object. Using remove() on the Gob allows chaining.
+ *
+ * @param  {feature} feature The feature to remove
+ */
+auk.Gob.prototype.removeFeature = function (feature) {
+
+	if (feature.removeFrom) {
+		feature.removeFrom(this);
+	}
+
+	return this;
+};
+
+/**
  * Adds a child gob. Removes it from it's former parent if necessary.
  *
  * @param  {gob} The gob to be added
@@ -61,17 +157,19 @@ auk.Gob.prototype.fireGameEvent = function (eventName) {
  */
 auk.Gob.prototype.addGob = function (gob) {
 	// Remove from existing parent
-	if (gob.parentGob && gob.parentGob.removeGob) {
-		gob.parentGob.removeGob(gob);
+	if (gob.parent && gob.parent.removeGob) {
+		gob.parent.removeGob(gob);
 	}
 
 	// Add the gob
 	gob.game = this.game;
-	gob.parentGob = this;
+	gob.parent = this;
 	this.push(gob);
 
-	if (gob.becomeChildGob) {
-		gob.becomeChildGob(this);
+	if (gob.fireGameEvent) {
+		gob.fireGameEvent('becomeChild');
+	} else if (gob.becomeChild) {
+		gob.becomeChild();
 	}
 
 	return this;
@@ -86,13 +184,17 @@ auk.Gob.prototype.removeGob = function (gob) {
 	var gobPos = this.indexOf(gob);
 
 	if (gobPos !== -1) {
+
+		if (gob.fireGameEvent) {
+			gob.fireGameEvent('becomeOrphan');
+		} else if (gob.becomeOrphan) {
+			gob.becomeOrphan();
+		}
+
 		delete gob.game;
-		delete gob.parentGob;
+		delete gob.parent;
 		this.splice(gobPos, 1);
 
-		if (gob.becomeOrphanGob) {
-			gob.becomeOrphanGob(this);
-		}
 	}
 
 	return this;
@@ -106,14 +208,17 @@ auk.Gob.prototype.removeGob = function (gob) {
  * The Game object is a special Gob that calls it's own fireGameEvent function,
  * and can initialize modules. It also stores the grid size, and the
  * global-esque storage bucket.
- *
- * @param display   The DOM object that will become the container for the game
- * @param grid      The size of the game's grid, in pixels.
+ * @param {int} width  Width of the game in px.
+ * @param {int} height Height of the game in px.
  */
-auk.Game = function (grid) {
+auk.Game = function (width, height) {
+	this.width = width;
+	this.height = height;
+	this.x = 0;
+	this.y = 0;
+	this.z = 0;
 
 	// Global game variables
-	this.grid = grid; // The size of 1 grid unit
 	this.loopSteps = ['update']; // Main loop sequence
 	this.bucket = {}; // Storage for game objects not in use.
 
